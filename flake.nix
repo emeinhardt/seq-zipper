e468dbc3a33e95d06a1a15c49e990afe43970cb4
Eric Meinhardt <ericmeinhardt@gmail.com>
2023-11-09 11:13:48 -0800

    Initial working haskell.nix flake setup

--- flake.nix
+++ flake.nix
@@ -1,51 +1,46 @@
 {
   description = "A Haskell package for a comonadic zipper backed by Sequence instead of a list.";
-
+  
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ] (system:
+    let
+      overlays = [ haskellNix.overlay
+        (final: prev: {
+          seq-zipper = 
+            final.haskell-nix.project' {
+              src = ./.;
+              compiler-nix-name = "ghc947";
+              shell.tools = {
+                cabal = {};
+                hlint = {};
+                haskell-language-server = {};
+              };
+              shell.buildInputs = with pkgs; [
+                # Optional: Only needed to make use of ./justfile
+                just
 
-        # Change 945 appropriately to select a different GHC version
-        # Check `nix-env -f "<nixpkgs>" -qaP -A haskell.compiler` to see available versions.
-        haskellPackages = pkgs.haskell.packages.ghc945;
+                # Optional: Only needed to make use of ./dev/cabal-gen-docs.sh
+                coreutils
+              ];
+            };
+        })
+      ];
+      pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+      flake = pkgs.seq-zipper.flake {
+      };
+    in flake // {
+      packages.default = flake.packages."seq-zipper:lib:seq-zipper";
+    });
 
-        jailbreakUnbreak = pkg:
-          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
-
-        packageName = "seq-zipper";
-      in {
-        packages.${packageName} =
-          haskellPackages.callCabal2nix packageName self rec {
-          };
-
-        packages.default = self.packages.${system}.${packageName};
-        defaultPackage = self.packages.${system}.default;
-
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-
-            cabal-install
-
-            haskellPackages.haskell-language-server
-
-            haskellPackages.hlint
-            haskellPackages.stylish-haskell
-            haskellPackages.hindent
-
-            # Optional: Only needed to make use of ./justfile
-            just
-
-            # Optional: Only needed to make use of ./dev/cabal-gen-docs.sh
-            coreutils
-          ];
-          inputsFrom = map (__getAttr "env") (__attrValues self.packages.${system});
-        };
-        devShell = self.devShells.${system}.default;
-      });
+  nixConfig = {
+    extra-substituters = ["https://cache.iog.io"];
+    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+    allow-import-from-derivation = "true";
+  };
 }
